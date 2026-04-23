5152755fff21cf61fed84125b611a4b80dad182c
teamaccrount-gif <teamaccrount@gmail.com>
2026-04-23 17:06:00 +0530

    update

--- Backend/routers/filter.routes.js
+++ Backend/routers/filter.routes.js
@@ -2,17 +2,18 @@ import express from "express";
 
 import { generateId } from "../middlewares/generateid.js";
 import { getAggrigationData, getDelta, getLatestData, getRawData, getTags } from "../controllers/filter.controller.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const router = express.Router();
 
-router.get("/tag", generateId, getTags);
+router.get("/tag", generateId, authenticate, getTags);
 
-router.post("/raw", generateId, getRawData);
+router.post("/raw", generateId, authenticate, getRawData);
 
-router.post("/aggregation", generateId, getAggrigationData);
+router.post("/aggregation", generateId, authenticate, getAggrigationData);
 
-router.post("/delta", generateId, getDelta);
+router.post("/delta", generateId, authenticate, getDelta);
 
-router.post("/latest", generateId, getLatestData);
+router.post("/latest", generateId, authenticate, getLatestData);
 
 export default router;
